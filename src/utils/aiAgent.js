42d72d024fecbd68fbcffcadd0f62c8e09830c8b
mahbub-sakib <mahbub.sakib418@gmail.com>
2026-06-13 13:08:42 +0600

    location file name updated

--- src/utils/aiAgent.js
+++ src/utils/aiAgent.js
@@ -1,5 +1,5 @@
 export async function getLocationFromAI(theme) {
-    const response = await fetch("/api/locations", {
+    const response = await fetch("/api/location", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ theme }),
